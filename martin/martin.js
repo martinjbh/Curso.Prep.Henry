3aa8bdf87438bd69b62a99b27dad51f21391386f
martin <barreiromartinj@gmail.com>
2021-11-14 00:25:18 -0300

    terminado face 3

---
+++ martin/martin.js
@@ -0,0 +1,21 @@
+
+// numeros pares
+
+
+function prueba (n) {
+    for (let i = 0; i < n; i++) {
+       if(i%2 === 0){
+           console.log(i)
+           
+       }
+        
+    }
+}
+
+prueba(200)
+
+//
+
+
+
+
