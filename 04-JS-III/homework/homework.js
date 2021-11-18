559d5c6a8bea029b651a8d63654bbc941bfde71c
martin <barreiromartinj@gmail.com>
2021-11-17 21:51:15 -0300

    05 listooo

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -135,7 +135,7 @@ function multiplicarArgumentos() {
   // Usa la palabra clave `arguments` para multiplicar todos los argumentos y devolver el producto
   // Si no se pasan argumentos devuelve 0. Si se pasa un argumento, simplemente devuélvelo
   // Escribe tu código aquí:
-
+ 
 
 
   if (arguments.length === 0) {
